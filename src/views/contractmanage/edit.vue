0c3cff517d7cc516c5bca1be601f29e7505a4309
Geng Peng <917589697@qq.com>
2019-12-19 15:53:29 +0800

    合同编辑修改

--- src/views/contractmanage/edit.vue
+++ src/views/contractmanage/edit.vue
@@ -175,11 +175,11 @@
             <p class="det_p">
               <span class="det_span">
                 合同总金额
-                <span style="color:#000000;">{{data.data.contractAmount}}</span>元
+                <span style="color:#000000;">{{thousandNum(data.data.contractAmount)||0}}</span>元
               </span>
               <span>
                 剩余款数
-                <span style="color:red">{{remainingMoney}}</span>元
+                <span style="color:red">{{thousandNum(remainingMoney)||0}}</span>元
               </span>
             </p>
             <div v-for="(item,index) in paymentList" :key="index">
@@ -203,16 +203,16 @@
                 </section>
                 <section class="zq_c zq_se" style="color:#797979;">
                   <p class="zq_p">本期应付（元）</p>
-                  <p>{{item.paymentAmount}}</p>
+                  <p>{{thousandNum(item.paymentAmount)||0}}</p>
                 </section>
                 <section class="zq_c zq_se" style="color:#797979;">
                   <p class="zq_p">本期实付（元）</p>
-                  <p>{{item.currentAmount}}</p>
+                  <p>{{thousandNum(item.currentAmount)||0}}</p>
                 </section>
                 <section class="zq_c zq_se" style="color:#797979;">
                   <p class="zq_p" v-if="item.currentTicketAmount > 0">已开票（元）</p>
                   <div style="display:flex;justify-content: center;">
-                    <p class="cor_span" style="margin-right:10px" v-if="item.currentTicketAmount>0">{{item.currentTicketAmount}}</p>
+                    <p class="cor_span" style="margin-right:10px" v-if="item.currentTicketAmount>0">{{thousandNum(item.currentTicketAmount)}}</p>
                     <!-- <p class="cor_span">
                       <Button size="small" @click="fpmodal = true" v-if="item.ticketButton">开发票</Button>
                     </p> -->
@@ -294,12 +294,16 @@
             <Table ref="currentRowTable" :columns="update_columns" :data="data.data.updateList"></Table>
           </TabPane>
         </Tabs>
+        <Modal v-model="orderDetailOpen" width="1000">
+          <orderDetail :orderNO="selectedOrder"></orderDetail>
+        </Modal>
       </content>
     </Layout>
   </div>
 </template>
 
 <script>
+import orderDetail from '../ordermanage/order-detail';
 const subjectName = {
   1:'电能云',
   2:'智慧能源',
@@ -323,6 +327,9 @@ const contractContentMap = {
 }
 export default {
   name: "htedit",
+  components:{
+    orderDetail
+  },
   data() {
     return {
       htxq: {
@@ -381,12 +388,15 @@ export default {
       projectmen:[],
       contractContentMap,
       fj:[],
-      uploadLoading:false
+      uploadLoading:false,
+      selectedOrder:'',
+      orderDetailOpen:false
     };
   },
   methods: {
     goOrderDetail(){
-      this.$router.push({path: "/ordermanage/orderManage",query:{orderNo:this.data.data.orderNo}})
+      this.selectedOrder = this.data.data.orderNo;
+      this.orderDetailOpen = true;
     },
     showPayment(index){
       this.$set(this.showObj,index,!this.showObj[index]);
@@ -525,7 +535,38 @@ export default {
           this.fj.push(item);
         });
       });
-    }
+    },
+    thousandNum(num){
+      if(num){
+        //将num中的$,去掉，将num变成一个纯粹的数据格式字符串
+        num = num.toString().replace(/\$|\,/g,'');
+        //如果num不是数字，则将num置0，并返回
+        if(''==num || isNaN(num)){return 'Not a Number ! ';}
+        //如果num是负数，则获取她的符号
+        var sign = num.indexOf("-")> 0 ? '-' : '';
+        //如果存在小数点，则获取数字的小数部分
+        var cents = num.indexOf(".")> 0 ? num.substr(num.indexOf(".")) : '';
+        cents = cents.length>1 ? cents : '' ;//注意：这里如果是使用change方法不断的调用，小数是输入不了的
+        //获取数字的整数数部分
+        num = num.indexOf(".")>0 ? num.substring(0,(num.indexOf("."))) : num ;
+        //如果没有小数点，整数部分不能以0开头
+        if('' == cents){ if(num.length>1 && '0' == num.substr(0,1)){return 'Not a Number ! ';}}
+        //如果有小数点，且整数的部分的长度大于1，则整数部分不能以0开头
+        else{if(num.length>1 && '0' == num.substr(0,1)){return 'Not a Number ! ';}}
+        //针对整数部分进行格式化处理，这是此方法的核心，也是稍难理解的一个地方，逆向的来思考或者采用简单的事例来实现就容易多了
+        /*
+          也可以这样想象，现在有一串数字字符串在你面前，如果让你给他家千分位的逗号的话，你是怎么来思考和操作的?
+          字符串长度为0/1/2/3时都不用添加
+          字符串长度大于3的时候，从右往左数，有三位字符就加一个逗号，然后继续往前数，直到不到往前数少于三位字符为止
+         */
+        for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+        {
+            num = num.substring(0,num.length-(4*i+3))+','+num.substring(num.length-(4*i+3));
+        }
+        //将数据（符号、整数部分、小数部分）整体组合返回
+        return (sign + num + cents); 
+      }
+    },
   },
   beforeCreate(){
     if(Object.keys(JSON.parse(localStorage.getItem('contractInfo'))||{}).length === 0) this.$router.push({path:'/contractmanage/contractmanage'});
