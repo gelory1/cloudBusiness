e3f3247f489a17e3dd8be074991384fc717ae40b
Geng Peng <917589697@qq.com>
2019-12-22 21:11:10 +0800

    报表

--- src/views/tables/dragable-table.vue
+++ src/views/tables/dragable-table.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <p>报表</p>
+    <div class="address layout">
+        <Layout style="background:#fff;min-height:800px">
+            <iframe style="margin-top:10px" src="https://das.base.shuju.aliyun.com/token3rd/dashboard/view/pc.htm?pageId=6e0d12cf-9b77-4c1e-abdf-188cfe3a6727&accessToken=a07b65da88573bddeccdc018c9fc463e" height="800"></iframe>
+        </Layout>
     </div>
 </template>
 
