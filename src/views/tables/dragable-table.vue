3d38b0146026718ea93bca7d50097f609e2fd060
Geng Peng <917589697@qq.com>
2019-12-25 09:36:46 +0800

    修改报表地址

--- src/views/tables/dragable-table.vue
+++ src/views/tables/dragable-table.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="address layout">
         <Layout style="background:#fff;min-height:800px">
-            <iframe style="margin-top:10px" src="https://das.base.shuju.aliyun.com/token3rd/dashboard/view/pc.htm?pageId=fce3a348-6c26-4921-b7e5-1402d81e055b&accessToken=ecd460c68d2e30f5857949571251d8ff" height="800"></iframe>
+            <iframe style="margin-top:10px" src="https://das.base.shuju.aliyun.com/token3rd/dashboard/view/pc.htm?pageId=6e0d12cf-9b77-4c1e-abdf-188cfe3a6727&accessToken=7e16eb98dcdb7c0396acfc4b2fba8e5b" height="800"></iframe>
         </Layout>
     </div>
 </template>
